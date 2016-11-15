1563e9878149755138b19821a86996c56a6600c1
Szymon Karnecki <ext.skarnecki@riotgames.com>
2016-11-15 12:12:51 +0100

    Initial Commit

---
+++ bar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  bar
+//
+//  Created by Szymon Karnecki on 15/11/16.
+//  Copyright © 2016 Szymon Karnecki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
